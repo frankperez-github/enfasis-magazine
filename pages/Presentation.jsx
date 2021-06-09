7ddaefda474d83922c5c2608726fcc7c8b3bb92d
Frank Pérez <fp848584@gmail.com>
2021-06-09 17:44:54 -0400

    Presentation Updated

--- pages/Presentation.jsx
+++ pages/Presentation.jsx
@@ -11,39 +11,39 @@ export default function Presentation(){
         <div className="Presentation">
             
             <div className="Networks">
-                
-                <Link href="https://www.instagram.com/enfasismag" > 
-                    <div className="Link">
-                        <Image src="/instagram_home.svg" width="100%" height="100%" />
-                    </div>
-                </Link>
-                <Link href="https://www.facebook.com/enfasismag" >
-                    <div className="Link">
-                        <Image src="/facebook_home.svg" width="100%" height="100%" />
-                    </div>
-                </Link>
-                <Link href="https://www.twitter.com/enfasismag" > 
-                    <div className="Link">
-                        <Image src="/twitter_home.svg" width="100%" height="100%" />
-                    </div>
-                </Link>
-                     
+                <div className="network-div">
+                    <Link href="https://www.instagram.com/enfasismag" > 
+                        <div className="Link">
+                            <Image src="/instagram_home.svg" width="100%" height="100%" />
+                        </div>
+                    </Link>
+                    <Link href="https://www.facebook.com/enfasismag" >
+                        <div className="Link">
+                            <Image src="/facebook_home.svg" width="100%" height="100%" />
+                        </div>
+                    </Link>
+                    <Link href="https://www.twitter.com/enfasismag" > 
+                        <div className="Link">
+                            <Image src="/twitter_home.svg" width="100%" height="100%" />
+                        </div>
+                    </Link>
+                </div>
             </div>
 
             <div className="Promo_Pic">
-
-                <div id="ed3">
-                    <Image src="/ed03_portada.jpg" width="400%" height="500%" />
+                <div className="pic-div">
+                    <div id="ed3">
+                        <Image src="/ed03_portada.jpg" width="400%" height="500%" />
+                    </div>
+                    
+                    <div id="ed2">
+                        <Image src="/ed02_portada.jpg" width="400%" height="500%" />
+                    </div>
+                    
+                    <div id="ed1">
+                        <Image src="/ed01_portada.jpg" width="400%" height="500%" />
+                    </div>
                 </div>
-                
-                <div id="ed2">
-                    <Image src="/ed02_portada.jpg" width="400%" height="500%" />
-                </div>
-                
-                <div id="ed1">
-                    <Image src="/ed01_portada.jpg" width="400%" height="500%" />
-                </div>
-
             </div>
 
             <div className="Nav_menu">
