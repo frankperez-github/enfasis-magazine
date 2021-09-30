d65ee47f9b8025029831e402a5a1897a868505c8
Frank Pérez <fp848584@gmail.com>
2021-09-30 12:33:50 -0400

    Presentation optimized

--- pages/Presentation.jsx
+++ pages/Presentation.jsx
@@ -1,80 +1,18 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Menu from '../components/MobileVersion/PresentationMenu';
 import PresentationImages from '../components/PresentationImages';
 
 
 
 export default function Presentation(){
 
+    function ShowMenu(){
+        document.getElementById('PresentationMenu').style.display="block";
+    }
    
     return(
         
-        <div className="Presentation">
-            
-            <div className="Networks">
-                <div className="network-div">
-                    <a href="https://www.instagram.com/enfasismag" target="_blank">
-                        <div className="Link">
-                            <Image src="/instagram_home.svg" width="100%" height="100%" />
-                        </div>
-                    </a>
-                
-                
-                    <a href="https://www.facebook.com/enfasismag" target="_blank">
-                        <div className="Link">
-                            <Image src="/facebook_home.svg" width="100%" height="100%" />
-                        </div>
-                    </a>
-                
-                
-                    <a href="https://www.twitter.com/enfasismag" target="_blank">
-                        <div className="Link">
-                            <Image src="/twitter_home.svg" width="100%" height="100%" />
-                        </div>
-                    </a>
-                </div>
-            </div>
-
-            <div className="Promo_Pic">
-                <div className="pic-div">
-                    <PresentationImages />
-                </div>
-            </div>
-
-            <div className="Nav_menu">
-                <div className="WhiteLogo">
-                    <Image src="/White_Logo.svg" width="200%" height="65%"/>
-                </div>
-                
-                <div className="White_Stick">
-                    <Image src="/White_Stick.svg" width="50%" height="80%" className="whiteStick"/>
-                </div>
-                
-                
-                <Link href="/Graphic_Design" >
-                    <p className="Link1 LinkSect">diseño gráfico </p>
-                </Link>
-
-                <Link href="/Industrial_Design" >
-                    <p className="Link2 LinkSect">diseño industrial</p>
-                </Link>
-
-                <Link href="/Architecture">
-                    <p className="Link3 LinkSect">arquitectura</p>
-                </Link>
-
-                <Link href="/Creators">
-                    <p className="Link4 LinkSect">creadores</p>
-                </Link>
-                
-                <Link href="/Magazine">
-                    <p className="Link5 LinkSect">revista</p>
-                </Link>
-                
-                
-            </div>
-            
-        </div>
-
+        
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import Presentation from "./Presentation";
-import Presentation_mob from "./Presentation_mob";
+import Image from 'next/image';
+import Link from 'next/link';
+import Menu from '../components/MobileVersion/PresentationMenu';
 import Head from 'next/head'
 
 
 export default function Home() {
+
+  function ShowMenu(){
+    document.getElementById('PresentationMenu').style.display="block";
+}
 
   return (
       <div>
@@ -16,9 +21,100 @@ export default function Home() {
         
 
         <main >
-          <Presentation />
+          <div className="Presentation">
 
-          <Presentation_mob />
+            <div className="Presentation_mob">
+
+                <Menu />
+
+                <div className="Nav_menu_Mob">
+                    <div className="WhiteLogoMob">
+                        <Image src="/White_Logo.svg" width="200%" height="65%"/>
+                    </div>
+                    <div className="Burger_menu" onClick={ShowMenu}>
+                        <Image src="/Burger_Menu.svg" width="100%" height="100%" />
+                    </div>
+                </div>
+            
+
+            
+                <div className="Networks">
+                    <div className="network-div">
+                        <a href="https://www.instagram.com/enfasismag" target="_blank">
+                            <div className="Link">
+                                <Image src="/instagram_home.svg" width="100%" height="100%" />
+                            </div>
+                        </a>
+                    
+                    
+                        <a href="https://www.facebook.com/enfasismag" target="_blank">
+                            <div className="Link">
+                                <Image src="/facebook_home.svg" width="100%" height="100%" />
+                            </div>
+                        </a>
+                    
+                    
+                        <a href="https://www.twitter.com/enfasismag" target="_blank">
+                            <div className="Link">
+                                <Image src="/twitter_home.svg" width="100%" height="100%" />
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="Promo_Pic_Mob">
+                <div className="pic-div">
+                  <div id="ed3">
+                      <Image src="/ed03_portada.jpg" width="400%" height="500%" />
+                  </div>
+                  
+                  <div id="ed2">
+                      <Image src="/ed02_portada.jpg" width="400%" height="500%" />
+                  </div>
+                  
+                  <div id="ed1">
+                      <Image src="/ed01_portada.jpg" width="400%" height="500%" />
+                  </div>
+                </div>
+            </div>
+
+            <div className="Nav_menu">
+                <div className="WhiteLogo">
+                    <Image src="/White_Logo.svg" width="200%" height="65%"/>
+                </div>
+                
+                <div className="White_Stick">
+                    <Image src="/White_Stick.svg" width="50%" height="80%" className="whiteStick"/>
+                </div>
+                
+                
+                <Link href="/Graphic_Design" >
+                    <p className="Link1 LinkSect">diseño gráfico </p>
+                </Link>
+
+                <Link href="/Industrial_Design" >
+                    <p className="Link2 LinkSect">diseño industrial</p>
+                </Link>
+
+                <Link href="/Architecture">
+                    <p className="Link3 LinkSect">arquitectura</p>
+                </Link>
+
+                <Link href="/Creators">
+                    <p className="Link4 LinkSect">creadores</p>
+                </Link>
+                
+                <Link href="/Magazine">
+                    <p className="Link5 LinkSect">revista</p>
+                </Link>
+                
+                
+            </div>
+            
+          </div>
+
+
         </main>
       </div>
   )
