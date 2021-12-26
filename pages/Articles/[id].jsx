42406a58c8f5f8678ef184d487ce8e2a3f820563
Adiazx14 <adrianvaldes1439@gmail.com>
2021-12-26 14:53:42 -0500

    google ads

--- pages/Articles/[id].jsx
+++ pages/Articles/[id].jsx
@@ -55,6 +55,8 @@ const Article = ({article, promotions, articles}) => {
           <meta name="keywords" content={article.keywords} />
           <meta name="description" content={article.description} />
           <link rel="icon" href="/favicon.ico" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8282269104002025"
+     crossOrigin="anonymous"></script>
         </Head>
         
 
